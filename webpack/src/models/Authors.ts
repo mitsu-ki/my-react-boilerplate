2fdac65f34563bd89f6ec13be28d72c81d010161
mitsu-ki <mitsuhiro.kine@4digit.jp>
2017-07-18 19:31:08 +0900

    [fix] 使用する形式に変換

--- webpack/src/models/Authors.ts
+++ webpack/src/models/Authors.ts
@@ -1,37 +1,22 @@
-import { Record, List } from 'immutable';
+interface IAuthor {
+  id      : number;
+  name    : string;
+  books_id: number[];
+}
 
-const AuthorsRecord = Record({
+export interface IAuthors {
+  authors: IAuthor[];
+}
+
+const AuthorsData: IAuthors = {
   authors: [
     {
       id      : 1,
-      name    : "村上春樹",
-      books_id: [
-        1,
-        3
-      ]
-    },
-    {
-      id      : 2,
-      name    : "村上龍",
-      books_id: [
-        2,
-        5
-      ]
-    },
-    {
-      id      : 3,
-      name    : "東野圭吾",
-      books_id: [
-        3
-      ]
+      name    : "initial author name",
+      books_id: []
     }
   ]
-});
+};
 
-class Authors extends AuthorsRecord {
-  getAuthors() {
-    return this.get('authors') || [];
-  }
-}
 
-export default Authors;
+export default AuthorsData;
